7867e70e1ac521b21154718ec23ccc5deaec3694
林思怡 <linsiyi@linsiyide-MacBook-Air.local>
2018-06-11 10:46:51 +0800

    version 1.0

--- src/dragEvent.js
+++ src/dragEvent.js
@@ -1,20 +1,36 @@
 function drag(ev) {
-  ev.dataTransfer.setData("div", ev.target);
+  ev.target.id = "drag";
+  ev.dataTransfer.setData("div", ev.target.id);
 }
 function createDiv(ev) {
   var check_new_div = document.getElementsByClassName("new-div");
   if (check_new_div.length === 0) {
     var this_task = ev.target;
     var new_div = document.createElement("DIV");
-    new_div.addEventListener("ondrop")
+    new_div.addEventListener("ondrop", drop(event));
+    new_div.addEventListener("ondragover", allowDrop(event));
     new_div.className = "new-div";
     ev.target.parentElement.parentElement.insertBefore(new_div, ev.target.parentElement);
     
   } else {
+    
+    
     check_new_div[0].remove();
     var this_task = ev.target;
     var new_div = document.createElement("DIV");
     new_div.className = "new-div";
     ev.target.parentElement.parentElement.insertBefore(new_div, ev.target.parentElement);
+    
   }
+}
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+function drop(ev) {
+  console.log("drop");
+  
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("div");
+  ev.target.appendChild(document.getElementById(data));
+  
 }
